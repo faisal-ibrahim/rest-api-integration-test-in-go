7afc95bd4dde08668a1975d9ee7d5b9392dcb82f
Faisal Ibrahim <faisal.im048@gmail.com>
2022-03-20 17:30:49 +0100

    Added first integration test

--- go.mod
+++ go.mod
@@ -9,6 +9,7 @@ require (
 
 require (
 	github.com/andybalholm/brotli v1.0.0 // indirect
+	github.com/davecgh/go-spew v1.1.0 // indirect
 	github.com/gofiber/utils v0.0.10 // indirect
 	github.com/gorilla/schema v1.1.0 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
@@ -16,8 +17,12 @@ require (
 	github.com/mattn/go-colorable v0.1.7 // indirect
 	github.com/mattn/go-isatty v0.0.12 // indirect
 	github.com/mattn/go-sqlite3 v1.14.0 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/stretchr/objx v0.1.0 // indirect
+	github.com/stretchr/testify v1.7.1 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasthttp v1.16.0 // indirect
 	github.com/valyala/tcplisten v0.0.0-20161114210144-ceec8f93295a // indirect
 	golang.org/x/sys v0.0.0-20200602225109-6fdc65e7d980 // indirect
+	gopkg.in/yaml.v3 v3.0.0-20200313102051-9f266ea9e77c // indirect
 )
--- internal/book/book_integration_test.go
+++ internal/book/book_integration_test.go
@@ -2,7 +2,20 @@
 
 package book_test
 
-import "github.com/faisal-ibrahim/rest-api-integration-test-in-go.git/internal/transport"
+import (
+	"github.com/faisal-ibrahim/rest-api-integration-test-in-go.git/internal/book"
+	"github.com/faisal-ibrahim/rest-api-integration-test-in-go.git/internal/database"
+	"github.com/faisal-ibrahim/rest-api-integration-test-in-go.git/internal/transport"
+	"github.com/gofiber/fiber"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/suite"
+	"net/http"
+	"net/http/httptest"
+	"os"
+	"strconv"
+	"strings"
+	"testing"
+)
 
 type BookTestSuite struct {
 	suite.Suite
@@ -11,4 +24,33 @@ type BookTestSuite struct {
 
 func (suite *BookTestSuite) SetupSuite() {
 	suite.app = transport.Setup()
+	database.InitDatabase()
+	database.DBConn.AutoMigrate(&book.Book{})
+}
+
+func (suite *BookTestSuite) TearDownSuite() {
+	err := os.Remove("books.db")
+	if err != nil {
+		panic("Failed to remove database")
+	}
+}
+
+func (suite *BookTestSuite) TestCreateBook() {
+	req := httptest.NewRequest(
+		"POST",
+		"/api/v1/book",
+		strings.NewReader(`{"ISIN": 12345, "title": "Test book", "author": "Faisal", "rating": 5}`),
+	)
+
+	req.Header.Add("Content-Type", "application/json")
+	req.Header.Add("Content-Length", strconv.FormatInt(req.ContentLength, 10))
+
+	res, err := suite.app.Test(req, -1)
+
+	assert.NoError(suite.T(), err)
+	assert.Equal(suite.T(), http.StatusOK, res.StatusCode)
+}
+
+func TestBookTestSuite(t *testing.T) {
+	suite.Run(t, new(BookTestSuite))
 }
